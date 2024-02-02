ef0f6418bd4649f366b842debc7340b3aff51dc6
tarunj292 <156983323+tarunj292@users.noreply.github.com>
2024-02-02 21:30:34 +0530

    inputEl, inputBTn ulEl created; renderLeads() created; inputBtn working

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-let inputBtn = document.getElementById("input")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-inputBtn.addEventListener("click", function(){
-    console.log("Button clicked!")
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
 })
+
+function renderLeads(){
+    let listItems = "" //using listTems to first have all element and then as loop executes manupulate dom at end because DOM Manupulation inside loop cost us higher
+    for (let i = 0; i < myLeads.length; i++) {
+    // listItems += "<li><a href=" + myLeads[i] + ">" + myLeads[i] + "</a></li>"
+    listItems += `
+        <li>
+            <a href= ${myLeads[i]} >
+                ${myLeads[i]} 
+            </a>
+        </li>
+    `
+    // create element
+    // set text content
+    // append to ul
+    // const li = document.createElement("li")
+    // li.textContent = myLeads[i]
+    // ulEl.append(li)
+}
+ulEl.innerHTML = listItems
+}
