089262cfd3c34efc26653a2450f1ccb333f269b7
tarunj292 <156983323+tarunj292@users.noreply.github.com>
2024-02-03 06:50:12 +0530

    double btn logic add dblclick

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     renderLeads()
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+    myLeads = []
+    localStorage.clear
+    renderLeads()
+})
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
